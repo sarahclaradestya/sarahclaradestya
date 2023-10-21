d6eb059befec194f28866f494684d6b0f32d6059
sarahclaradestya <147043134+sarahclaradestya@users.noreply.github.com>
2023-10-21 20:25:31 +0700

    Add files via upload

---
+++ tugas-2/tugas mandiri 1 pertemuan 2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int alas;
+	int tinggi;
+	float luas;
+	
+	alas = 8;
+	tinggi = 5;
+	luas = 0.5 * alas * tinggi;
+	
+	printf(" luas segitiga adalah : %f", luas);
+	return 0;
+}
