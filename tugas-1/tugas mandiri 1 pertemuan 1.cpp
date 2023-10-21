b635e11cbdbc497b69c1f2d77b6d034a54794e1f
sarahclaradestya <147043134+sarahclaradestya@users.noreply.github.com>
2023-10-21 20:24:50 +0700

    Add files via upload

---
+++ tugas-1/tugas mandiri 1 pertemuan 1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int nim = 3242;
+	char* nama = "david";
+	char* jurusan = "informatika";
+	
+	printf("nim : %d \n", nim);
+	printf("nama : %s \n", nama);
+	printf("jurusan : %s ", jurusan);
+	return 0;
+}
